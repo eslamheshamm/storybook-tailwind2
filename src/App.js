8cad109fac10bf46176bdd6e84eb9c6b777e641e
eslamheshamm <esl2mhesham@gmail.com>
2021-01-03 23:41:34 +0200

    import tailwind & the button component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "./components/Atoms/Buttons/Primary";
+import "./tailwind.css";
+
+function App() {
+	return (
+		<div className="App  text-center">
+			<h1 className="text-4xl font-bold pt-20">Hello World</h1>
+			<Button>Button Text</Button>
+		</div>
+	);
+}
+
+export default App;
